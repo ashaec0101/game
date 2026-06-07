9d736cb6b4b2b5e7dfead92a598bc3976e90406c
ashaec0101 <ashaec0101@gmail.com>
2026-06-06 18:18:08 -0700

    Fixed grammar and added random number of fruits to pick up.

--- game.py
+++ game.py
@@ -1,3 +1,3 @@
 import random
-thistuple = ("apple", "banana", "cherry","pineapple","grapes","apricot","peach","plum","watermelon","mango")
-print(f"I picked up this {thistuple[random.randint(0,9)]} fruit in the basket")
+thistuple = ("apple", "banana", "cherry","pineapple","grape","apricot","peach","plum","watermelon","mango")
+print(f"I picked up {random.randint(0,100)} {thistuple[random.randint(0,9)]} fruit from the basket")
