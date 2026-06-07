0c87ccf5a4cb1e7873911520d7ef6236b5c78c35
ashaec0101 <ashaec0101@gmail.com>
2026-06-06 17:33:09 -0700

    code included

---
+++ game.py
@@ -0,0 +1,3 @@
+import random
+thistuple = ("apple", "banana", "cherry","pineapple","grapes","apricot","peach","plum","watermelon","mango")
+print(f"I picked up this {thistuple[random.randint(0,9)]} fruit in the basket")
